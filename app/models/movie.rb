d23a9e4f9ad4763364a76501c8e0ec3eec97fc54
Allan Delarosa <ajd2215@columbia.edu>
2020-02-27 02:12:04 -0500

    Changed Movie.rating from hardcoded ratings to actually look for what ratings are currently in the database; also, no longer case sensitive when filtering for ratings

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,9 +1,14 @@
 class Movie < ActiveRecord::Base
+    # returns an array of ratings currently in the database
     def self.ratings
-        return ['G','PG','PG-13','R']
+        ratings = []
+        Movie.all.each do |movie|
+            ratings += [movie.rating.upcase]
+        end
+        return ratings.uniq
     end
 
     def self.with_ratings(ratings)
-        return Movie.where("rating IN (?)", ratings)
+        return self.where("upper(rating) IN (?)", ratings)
     end
 end
